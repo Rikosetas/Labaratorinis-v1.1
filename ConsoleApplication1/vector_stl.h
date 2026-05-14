01bb54ebf508ee2d7cd5a39164147d3839a073f2
Security <91418733+Rikosetas@users.noreply.github.com>
2026-05-15 02:18:57 +0300

    Vector: pasalintas erase(first,last) ir laisvasis swap, atnaujinti testai

--- ConsoleApplication1/vector_stl.h
+++ ConsoleApplication1/vector_stl.h
@@ -12,20 +12,20 @@
  * @brief Sablono klase Vector<T>, atkartojanti std::vector funkcionaluma.
  */
 
-/**
- * @brief Dinaminis masyvas, atkartojantis std::vector elgesi.
- *
- * Klase realizuoja: Rule of Five (kopijavimo / perkelimo ctor + operator=),
- * elementu prieigos metodus (operator[], at, front, back, data),
- * dydzio valdyma (size, capacity, empty, reserve, resize, shrink_to_fit, clear),
- * keitimo metodus (push_back, pop_back, insert, erase, emplace, emplace_back, swap),
- * iteratorius (begin, end, cbegin, cend) ir lyginimo operatorius.
- *
- * Papildomai realizuotas reallocation skaitliukas - @ref realloc_count() grazina
- * kiek kartu vidiniu buferiu perskirstymas (capacity perskaiciavimas) buvo atliktas.
- *
- * @tparam T elemento tipas.
- */
+ /**
+  * @brief Dinaminis masyvas, atkartojantis std::vector elgesi.
+  *
+  * Klase realizuoja: Rule of Five (kopijavimo / perkelimo ctor + operator=),
+  * elementu prieigos metodus (operator[], at, front, back, data),
+  * dydzio valdyma (size, capacity, empty, reserve, resize, shrink_to_fit, clear),
+  * keitimo metodus (push_back, pop_back, insert, erase, emplace, emplace_back, swap),
+  * iteratorius (begin, end, cbegin, cend) ir lyginimo operatorius.
+  *
+  * Papildomai realizuotas reallocation skaitliukas - @ref realloc_count() grazina
+  * kiek kartu vidiniu buferiu perskirstymas (capacity perskaiciavimas) buvo atliktas.
+  *
+  * @tparam T elemento tipas.
+  */
 template<typename T>
 class Vector {
 private:
@@ -35,80 +35,74 @@ private:
     size_t realloc_count_ = 0;
 
 public:
-    using value_type      = T;
-    using size_type       = size_t;
-    using reference       = T&;
-    using const_reference = const T&;
-    using pointer         = T*;
-    using const_pointer   = const T*;
-    using iterator        = T*;
-    using const_iterator  = const T*;
+    using iterator = T*;
+    using const_iterator = const T*;
 
 public:
     /// @brief Default konstruktorius - tuscias vektorius.
-    Vector() : data_(nullptr), size_(0), capacity_(0), realloc_count_(0) {}
+    Vector( ) : data_( nullptr ), size_( 0 ), capacity_( 0 ), realloc_count_( 0 ) { }
 
     /**
      * @brief Konstruktorius su pradiniu dydziu.
      * @param count elementu skaicius.
      * @param value pradine reiksme (default = T()).
      */
-    explicit Vector(size_t count, const T& value = T())
-        : data_(nullptr), size_(0), capacity_(0), realloc_count_(0)
+    explicit Vector( size_t count, const T& value = T( ) ) : data_( nullptr ), size_( 0 ), capacity_( 0 ), realloc_count_( 0 )
     {
-        if (count > 0) {
-            reserve(count);
-            for (size_t i = 0; i < count; i++)
-                data_[i] = value;
+        if ( count > 0 )
+        {
+            reserve( count );
+
+            for ( size_t i = 0; i < count; i++ )
+                data_ [ i ] = value;
+
             size_ = count;
         }
     }
 
     /// @brief Konstruktorius is initializer_list (Vector<int> v = {1,2,3}).
-    Vector(std::initializer_list<T> il)
-        : data_(nullptr), size_(0), capacity_(0), realloc_count_(0)
+    Vector( std::initializer_list<T> il ) : data_( nullptr ), size_( 0 ), capacity_( 0 ), realloc_count_( 0 )
     {
-        reserve(il.size());
-        for (const auto& v : il)
-            data_[size_++] = v;
+        reserve( il.size( ) );
+
+        for ( const auto& v : il )
+            data_ [ size_++ ] = v;
     }
 
     /// @brief Destruktorius.
-    ~Vector() { delete[] data_; }
+    ~Vector( ) { delete[ ] data_; }
 
     /// @brief Kopijavimo konstruktorius.
-    Vector(const Vector& other)
-        : data_(nullptr), size_(other.size_), capacity_(other.capacity_),
-          realloc_count_(0)
+    Vector( const Vector& other ) : data_( nullptr ), size_( other.size_ ), capacity_( other.capacity_ ), realloc_count_( 0 )
     {
-        if (capacity_ > 0) {
-            data_ = new T[capacity_];
-            for (size_t i = 0; i < size_; i++)
-                data_[i] = other.data_[i];
+        if ( capacity_ > 0 )
+        {
+            data_ = new T [ capacity_ ];
+
+            for ( size_t i = 0; i < size_; i++ )
+                data_ [ i ] = other.data_ [ i ];
         }
     }
 
     /// @brief Kopijavimo priskyrimas.
-    Vector& operator=(const Vector& other)
+    Vector& operator=( const Vector& other )
     {
-        if (this == &other) return *this;
+        if ( this == &other ) return *this;
 
-        delete[] data_;
+        delete[ ] data_;
 
         size_ = other.size_;
         capacity_ = other.capacity_;
-        data_ = capacity_ > 0 ? new T[capacity_] : nullptr;
+        data_ = capacity_ > 0 ? new T [ capacity_ ] : nullptr;
 
-        for (size_t i = 0; i < size_; i++)
-            data_[i] = other.data_[i];
+        for ( size_t i = 0; i < size_; i++ )
+            data_ [ i ] = other.data_ [ i ];
 
         return *this;
     }
 
     /// @brief Perkelimo konstruktorius.
-    Vector(Vector&& other) noexcept
-        : data_(other.data_), size_(other.size_), capacity_(other.capacity_),
-          realloc_count_(other.realloc_count_)
+    Vector( Vector&& other ) noexcept : data_( other.data_ ), size_( other.size_ ), capacity_( other.capacity_ ), realloc_count_( other.realloc_count_ )
     {
         other.data_ = nullptr;
         other.size_ = 0;
@@ -117,11 +111,11 @@ public:
     }
 
     /// @brief Perkelimo priskyrimas.
-    Vector& operator=(Vector&& other) noexcept
+    Vector& operator=( Vector&& other ) noexcept
     {
-        if (this == &other) return *this;
+        if ( this == &other ) return *this;
 
-        delete[] data_;
+        delete[ ] data_;
 
         data_ = other.data_;
         size_ = other.size_;
@@ -138,50 +132,50 @@ public:
 
 public:
     /// @brief Lyginimo operatorius == (elementu palyginimas).
-    bool operator==(const Vector& other) const
+    bool operator==( const Vector& other ) const
     {
-        if (size_ != other.size_)
+        if ( size_ != other.size_ )
             return false;
 
-        for (size_t i = 0; i < size_; i++)
-            if (data_[i] != other.data_[i])
+        for ( size_t i = 0; i < size_; i++ )
+            if ( data_ [ i ] != other.data_ [ i ] )
                 return false;
 
         return true;
     }
 
     /// @brief Lyginimo operatorius < (leksikografinis).
-    bool operator<(const Vector& other) const
+    bool operator<( const Vector& other ) const
     {
-        for (size_t i = 0; i < size_ && i < other.size_; i++)
+        for ( size_t i = 0; i < size_ && i < other.size_; i++ )
         {
-            if (data_[i] < other.data_[i]) return true;
-            if (data_[i] > other.data_[i]) return false;
+            if ( data_ [ i ] < other.data_ [ i ] ) return true;
+            if ( data_ [ i ] > other.data_ [ i ] ) return false;
         }
         return size_ < other.size_;
     }
 
-    bool operator!=(const Vector& other) const { return !(*this == other); }
-    bool operator<=(const Vector& other) const { return !(other < *this); }
-    bool operator>(const Vector& other)  const { return other < *this; }
-    bool operator>=(const Vector& other) const { return !(*this < other); }
+    bool operator!=( const Vector& other ) const { return !( *this == other ); }
+    bool operator<=( const Vector& other ) const { return !( other < *this ); }
+    bool operator>( const Vector& other )  const { return other < *this; }
+    bool operator>=( const Vector& other ) const { return !( *this < other ); }
 
 private:
     /**
      * @brief Vidinis buferiu perskirstymas.
      * @param wanted norimas naujas capacity (0 = automatinis x2 augimas).
      */
-    void internal_grow(size_t wanted = 0)
+    void internal_grow( size_t wanted = 0 )
     {
         size_t new_capacity = wanted == 0
-            ? (capacity_ == 0 ? 1 : capacity_ * 2)
+            ? ( capacity_ == 0 ? 1 : capacity_ * 2 )
             : wanted;
 
-        T* new_data = new T[new_capacity];
-        for (size_t i = 0; i < size_; i++)
-            new_data[i] = std::move(data_[i]);
+        T* new_data = new T [ new_capacity ];
+        for ( size_t i = 0; i < size_; i++ )
+            new_data [ i ] = std::move( data_ [ i ] );
 
-        delete[] data_;
+        delete[ ] data_;
         data_ = new_data;
         capacity_ = new_capacity;
 
@@ -190,136 +184,146 @@ private:
 
 public:
     /// @brief Elemento prieiga be ribu tikrinimo.
-    T& operator[](size_t index)             { return data_[index]; }
-    const T& operator[](size_t index) const { return data_[index]; }
+    T& operator[]( size_t index ) { return data_ [ index ]; }
+    const T& operator[]( size_t index ) const { return data_ [ index ]; }
 
     /// @brief Elemento prieiga su ribu tikrinimu (gali mesti std::out_of_range).
-    T& at(size_t index)
+    T& at( size_t index )
     {
-        if (index >= size_)
-            throw std::out_of_range("Vector::at index out of range");
-        return data_[index];
+        if ( index >= size_ )
+            throw std::out_of_range( "Vector::at index out of range" );
+
+        return data_ [ index ];
     }
 
-    const T& at(size_t index) const
+    const T& at( size_t index ) const
     {
-        if (index >= size_)
-            throw std::out_of_range("Vector::at index out of range");
-        return data_[index];
+        if ( index >= size_ )
+            throw std::out_of_range( "Vector::at index out of range" );
+
+        return data_ [ index ];
     }
 
-    T& front()             { return data_[0]; }
-    const T& front() const { return data_[0]; }
+    T& front( ) { return data_ [ 0 ]; }
+    const T& front( ) const { return data_ [ 0 ]; }
 
-    T& back()              { return data_[size_ - 1]; }
-    const T& back()  const { return data_[size_ - 1]; }
+    T& back( ) { return data_ [ size_ - 1 ]; }
+    const T& back( )  const { return data_ [ size_ - 1 ]; }
 
-    T* data()              { return data_; }
-    const T* data() const  { return data_; }
+    T* data( ) { return data_; }
+    const T* data( ) const { return data_; }
 
     /// @brief Tikrina ar vektorius tuscias.
-    bool empty() const { return size_ == 0; }
+    bool empty( ) const { return size_ == 0; }
 
     /// @brief Grazina elementu skaiciu.
-    size_t size() const { return size_; }
+    size_t size( ) const { return size_; }
 
     /// @brief Grazina capacity (kiek elementu telpa be reallocation).
-    size_t capacity() const { return capacity_; }
+    size_t capacity( ) const { return capacity_; }
 
     /// @brief Maksimalus galimas elementu skaicius.
-    size_t max_size() const { return std::numeric_limits<size_t>::max() / sizeof(T); }
+    size_t max_size( ) const { return std::numeric_limits<size_t>::max( ) / sizeof( T ); }
 
     /// @brief Grazina kiek kartu ivyko vidinis perskirstymas.
-    size_t realloc_count() const { return realloc_count_; }
+    size_t realloc_count( ) const { return realloc_count_; }
 
     /**
      * @brief Rezervuoja vietos bent new_cap elementu, jei dar nera.
      * @param new_cap norima capacity reiksme.
      */
-    void reserve(size_t new_cap)
+    void reserve( size_t new_cap )
     {
-        if (new_cap <= capacity_) return;
-        internal_grow(new_cap);
+        if ( new_cap <= capacity_ ) return;
+        internal_grow( new_cap );
     }
 
     /**
      * @brief Sumazina capacity iki size (jei reikia).
      */
-    void shrink_to_fit()
+    void shrink_to_fit( )
     {
-        if (size_ == capacity_) return;
+        if ( size_ == capacity_ ) return;
 
-        if (size_ == 0) {
-            delete[] data_;
+        if ( size_ == 0 )
+        {
+            delete[ ] data_;
             data_ = nullptr;
+
             capacity_ = 0;
             realloc_count_++;
             return;
         }
 
-        T* new_data = new T[size_];
-        for (size_t i = 0; i < size_; i++)
-            new_data[i] = std::move(data_[i]);
+        T* new_data = new T [ size_ ];
 
-        delete[] data_;
+        for ( size_t i = 0; i < size_; i++ )
+            new_data [ i ] = std::move( data_ [ i ] );
+
+        delete[ ] data_;
+
         data_ = new_data;
         capacity_ = size_;
         realloc_count_++;
     }
 
 public:
-    iterator       begin()        { return data_; }
-    iterator       end()          { return data_ + size_; }
-    const_iterator begin()  const { return data_; }
-    const_iterator end()    const { return data_ + size_; }
-    const_iterator cbegin() const { return data_; }
-    const_iterator cend()   const { return data_ + size_; }
+    iterator       begin( ) { return data_; }
+    iterator       end( ) { return data_ + size_; }
+
+    const_iterator begin( )  const { return data_; }
+    const_iterator end( )    const { return data_ + size_; }
+
+    const_iterator cbegin( ) const { return data_; }
+    const_iterator cend( )   const { return data_ + size_; }
 
 public:
     /// @brief Prideda elementa i pabaiga (kopija).
-    void push_back(const T& value)
+    void push_back( const T& value )
     {
-        if (size_ == capacity_)
-            internal_grow();
-        data_[size_++] = value;
+        if ( size_ == capacity_ )
+            internal_grow( );
+
+        data_ [ size_++ ] = value;
     }
 
     /// @brief Prideda elementa i pabaiga (perkelimas).
-    void push_back(T&& value)
+    void push_back( T&& value )
     {
-        if (size_ == capacity_)
-            internal_grow();
-        data_[size_++] = std::move(value);
+        if ( size_ == capacity_ )
+            internal_grow( );
+        data_ [ size_++ ] = std::move( value );
     }
 
     /// @brief Pasalina paskutini elementa.
-    void pop_back()
+    void pop_back( )
     {
-        if (empty())
-            throw std::runtime_error("Vector::pop_back on empty vector");
+        if ( empty( ) )
+            throw std::runtime_error( "Vector::pop_back on empty vector" );
+
         size_--;
     }
 
     /// @brief Istusina vektoriu (size_ = 0, capacity nepasikeicia).
-    void clear() { size_ = 0; }
+    void clear( ) { size_ = 0; }
 
     /**
      * @brief Iterpia elementa pries pos pozicija.
      * @return iteratorius i naujai iterpta elementa.
      */
-    iterator insert(const_iterator pos, const T& value)
+    iterator insert( const_iterator pos, const T& value )
     {
         size_t index = pos - data_;
-        if (index > size_)
-            throw std::out_of_range("Vector::insert index out of range");
+        if ( index > size_ )
+            throw std::out_of_range( "Vector::insert index out of range" );
 
-        if (size_ == capacity_)
-            internal_grow();
+        if ( size_ == capacity_ )
+            internal_grow( );
 
-        for (size_t i = size_; i > index; i--)
-            data_[i] = std::move(data_[i - 1]);
+        for ( size_t i = size_; i > index; i-- )
+            data_ [ i ] = std::move( data_ [ i - 1 ] );
 
-        data_[index] = value;
+        data_ [ index ] = value;
         size_++;
         return data_ + index;
     }
@@ -328,101 +332,79 @@ public:
      * @brief Pasalina elementa pos pozicijoje.
      * @return iteratorius i kita elementa po pasalintojo.
      */
-    iterator erase(const_iterator pos)
+    iterator erase( const_iterator pos )
     {
         size_t index = pos - data_;
-        if (index >= size_)
-            throw std::out_of_range("Vector::erase index out of range");
+        if ( index >= size_ )
+            throw std::out_of_range( "Vector::erase index out of range" );
 
-        for (size_t i = index; i + 1 < size_; i++)
-            data_[i] = std::move(data_[i + 1]);
+        for ( size_t i = index; i + 1 < size_; i++ )
+            data_ [ i ] = std::move( data_ [ i + 1 ] );
 
         size_--;
         return data_ + index;
     }
 
     /**
-     * @brief Pasalina elementus [first, last) intervale.
-     * @return iteratorius i kita elementa po intervalo.
-     */
-    iterator erase(const_iterator first, const_iterator last)
-    {
-        size_t start = first - data_;
-        size_t stop  = last  - data_;
-        if (start > size_ || stop > size_ || start > stop)
-            throw std::out_of_range("Vector::erase range out of range");
-
-        size_t count = stop - start;
-        for (size_t i = stop; i < size_; i++)
-            data_[i - count] = std::move(data_[i]);
-
-        size_ -= count;
-        return data_ + start;
-    }
-
-    /**
      * @brief Iterpia konstrukuojant elementa pries pos pozicija (perfect forwarding).
      */
     template<typename ... Args>
-    iterator emplace(const_iterator pos, Args&&... args)
+    iterator emplace( const_iterator pos, Args&&... args )
     {
         size_t index = pos - data_;
-        if (index > size_)
-            throw std::out_of_range("Vector::emplace index out of range");
+        if ( index > size_ )
+            throw std::out_of_range( "Vector::emplace index out of range" );
 
-        if (size_ == capacity_)
-            internal_grow();
+        if ( size_ == capacity_ )
+            internal_grow( );
 
-        for (size_t i = size_; i > index; i--)
-            data_[i] = std::move(data_[i - 1]);
+        for ( size_t i = size_; i > index; i-- )
+            data_ [ i ] = std::move( data_ [ i - 1 ] );
 
-        data_[index] = T(std::forward<Args>(args)...);
+        data_ [ index ] = T( std::forward<Args>( args )... );
         size_++;
         return data_ + index;
     }
 
     /// @brief Konstrukuoja elementa pabaigoje (perfect forwarding).
     template<typename ... Args>
-    void emplace_back(Args&&... args)
+    void emplace_back( Args&&... args )
     {
-        if (size_ == capacity_)
-            internal_grow();
+        if ( size_ == capacity_ )
+            internal_grow( );
 
-        data_[size_] = T(std::forward<Args>(args)...);
+        data_ [ size_ ] = T( std::forward<Args>( args )... );
         size_++;
     }
 
     /**
      * @brief Pakeicia dydi i count, ipildo value, jei reikia padidinti.
      */
-    void resize(size_t count, const T& value = T())
+    void resize( size_t count, const T& value = T( ) )
     {
-        if (count == size_) return;
+        if ( count == size_ ) return;
 
-        if (count < size_) {
+        if ( count < size_ )
+        {
             size_ = count;
             return;
         }
 
-        if (count > capacity_)
-            internal_grow(count);
+        if ( count > capacity_ )
+            internal_grow( count );
 
-        for (size_t i = size_; i < count; i++)
-            data_[i] = value;
+        for ( size_t i = size_; i < count; i++ )
+            data_ [ i ] = value;
 
         size_ = count;
     }
 
     /// @brief Sukeicia dvieju vektoriu turini.
-    void swap(Vector& other) noexcept
+    void swap( Vector& other ) noexcept
     {
-        std::swap(data_, other.data_);
-        std::swap(size_, other.size_);
-        std::swap(capacity_, other.capacity_);
-        std::swap(realloc_count_, other.realloc_count_);
+        std::swap( data_, other.data_ );
+        std::swap( size_, other.size_ );
+        std::swap( capacity_, other.capacity_ );
+        std::swap( realloc_count_, other.realloc_count_ );
     }
 };
-
-/// @brief Free funkcija swap (ADL palaikymui).
-template<typename T>
-inline void swap(Vector<T>& a, Vector<T>& b) noexcept { a.swap(b); }
